36ca016650c2be09c4dd383912f988adc7ee9c12
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-17 10:09:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  pigeon_list = {}
+  data.each do |attribute_key, value|
+    value.each do |attribute_value, name_list|
+      name_list.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][attribute_key]
+          pigeon_list[name][attribute_key] = []
+        end
+        pigeon_list[name][attribute_key] << attribute_value.to_s
+      end
+    end
+  end
+  pigeon_list
+  
 end
