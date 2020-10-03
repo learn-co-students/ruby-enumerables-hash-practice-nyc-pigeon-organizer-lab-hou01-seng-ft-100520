412269a7aa9a57f796807ce9d27733ca401e0c87
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-03 02:54:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  new_data = data.each_with_object({}) do |(key, value), new_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        #binding.pry
+        if !new_array[name]
+          new_array[name] = {}
+        end
+        if !new_array[name][key]
+            new_array[name][key] = []
+        end
+        new_array[name][key].push(inner_key)
+      end
+    end
+  end
+  binding.pry
 end
